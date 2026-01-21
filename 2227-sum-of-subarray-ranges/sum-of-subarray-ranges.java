760dae912200a83fe49642293ed957e6d5548f4d
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-01-21 22:14:32 +0530

    Time: 44 ms (18.67%) | Memory: 46.8 MB (29.16%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,17 +1,109 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        int n=nums.length;
-         long sum=0;
+    public static int[] getPse(int[] arr){
+        int n=arr.length;
+        int[] pse= new int[n];
+        Stack<Integer> st=new Stack<>();
         for(int i=0;i<n;i++){
-            long lar=nums[i];
-            long small=nums[i];
-            for(int j=i+1;j<n;j++){
-                lar=Math.max(lar,nums[j]);
-                small=Math.min(small,nums[j]);
-               long tsum=lar-small;
-                sum+=tsum;
+           int current=arr[i];
+            while(!st.isEmpty()&& current<arr[st.peek()]){
+                st.pop();
             }
+            if(st.isEmpty()){
+                pse[i]=-1;
+            }
+            else{
+                pse[i]=st.peek();
+            }
+            st.push(i);
+        }
+      return pse;
+    }
+    public static int[] getNse(int[] arr){
+        int n=arr.length;
+        int[] nse= new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+           int current=arr[i];
+            while(!st.isEmpty()&& current<=arr[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                nse[i]=n;
+            }
+            else{
+                nse[i]=st.peek();
+            }
+            st.push(i);
+        }
+      return nse;
+
+    }
+    public static long subarrayMin(int[] arr){
+        int[] pse=getPse(arr);
+        int[] nse=getNse(arr);
+        long sum=0;
+        for(int i=0;i<arr.length;i++){
+            long left=i-pse[i];
+            long right=nse[i]-i;
+            sum+=right*left*arr[i];
         }
         return sum;
+    }
+
+    public static int[] getPge(int[] arr){
+        int n=arr.length;
+        int[] pge=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+           int current=arr[i];
+            while(!st.isEmpty()&& current>arr[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                pge[i]=-1;
+            }
+            else{
+                pge[i]=st.peek();
+            }
+            st.push(i);
+        }
+      return pge;
+    }
+    public static int[] getNge(int[] arr){
+        int n=arr.length;
+        int[] nge=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+           int current=arr[i];
+            while(!st.isEmpty()&& current>=arr[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                nge[i]=n;
+            }
+            else{
+                nge[i]=st.peek();
+            }
+            st.push(i);
+        }
+      return nge;
+
+    }
+    public static long subarrayMax(int[] arr){
+        int[] pge=getPge(arr);
+        int[] nge=getNge(arr);
+        long sum=0;
+        for(int i=0;i<arr.length;i++){
+            long left=i-pge[i];
+            long right=nge[i]-i;
+            sum+=right*left*arr[i];
+        }
+        return sum;
+    }
+
+    public long subArrayRanges(int[] arr) {
+        long sumMin=subarrayMin(arr);
+        long sumMax=subarrayMax(arr);
+        return sumMax-sumMin;
     }
 }
