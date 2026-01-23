0ef49a0d4112c0b46ebd5f7b937195d10d0d3758
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-01-23 22:16:26 +0530

    Time: 21 ms (60.16%) | Memory: 48.3 MB (10.76%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -26,6 +26,10 @@ class Solution {
         while (ans.length() > 1 && ans.charAt(0) == '0') {
             ans.deleteCharAt(0);
         }
-        return ans.length()==0 ? "0" :ans.toString();
+        if(ans.length()==0){
+            return "0";
+
+        }
+        return ans.toString();
     }
 }
