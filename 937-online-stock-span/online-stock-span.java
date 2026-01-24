d9cbc0dda128e71c1fa969e22ab5268180305caf
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-01-24 13:58:20 +0530

    Time: 31 ms (57.98%) | Memory: 54.7 MB (28.26%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,22 +1,29 @@
 class StockSpanner {
      Stack<int[]> st; //{price,index}
-     int indx=-1;
+    //  int indx=-1;
     public StockSpanner() {
        
         st=new Stack<>();
     }
     
     public int next(int price) {
-        indx++;
+        int span =1;
+        // indx++;
         //pge logic
-        while(!st.isEmpty()&&st.peek()[0]<=price){
+       //po all smaller and add its span 
+        while(!st.isEmpty()&&st.peek()[0]<=price){ 
+            span+=st.peek()[1];
             st.pop();
         }
-        int pgeind=st.isEmpty() ? -1 : st.peek()[1];
-      int span =indx-pgeind;
-      st.push(new int[]{price,indx});
-      return span;
+    //     int pgeind=st.isEmpty() ? -1 : st.peek()[1];
+    //   int span =indx-pgeind;
+    //   st.push(new int[]{price,indx});
+    //   return span;
+    //push current price with its span
+        st.push(new int[]{price,span});
+    return span;
     }
+   
 }
 
 
