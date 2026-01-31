1617569247037de1a1a77e6e866d20f6460ca79e
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-01-31 20:02:21 +0530

    Time: 1 ms (87.28%) | Memory: 45.9 MB (16.53%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,26 +1,35 @@
 class Solution {
-    public static void permutation(int[] arr,List<Integer> temp,List<List<Integer>> ans,boolean[] mp){
-        if(temp.size()==arr.length){
+
+    public static void permutation(int idx, int[] arr, List<Integer> temp ,List<List<Integer>> ans) {
+
+        if (idx == arr.length) {
             ans.add(new ArrayList<>(temp));
             return;
         }
-        for(int i=0;i<arr.length;i++){
-            if(mp[i]){
-                continue;
-            }
-            temp.add(arr[i]);
-            mp[i]=true;
-            permutation(arr,temp,ans,mp);
-            //backtrack
+
+        for (int i = idx; i < arr.length; i++) {
+
+            // swap
+            int t = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = t;
+
+            temp.add(arr[idx]);
+            permutation(idx + 1, arr, temp, ans);
+
+            // backtrack
             temp.remove(temp.size() - 1);
-            mp[i] = false;
+
+            // swap back
+            t = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = t;
         }
     }
+
     public List<List<Integer>> permute(int[] nums) {
-         List<List<Integer>> ans=new ArrayList<>();
-         List<Integer> temp=new ArrayList<>();
-         boolean[] mp=new boolean[nums.length];
-         permutation(nums,temp,ans,mp);
-         return ans;
+        List<List<Integer>> ans = new ArrayList<>();
+        permutation(0, nums, new ArrayList<>(), ans);
+        return ans;
     }
 }
