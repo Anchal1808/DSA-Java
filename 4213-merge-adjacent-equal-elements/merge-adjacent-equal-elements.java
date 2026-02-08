6a527a7f8bb155700f422a7639d8938e5067af9e
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-02-08 21:26:24 +0530

    Time: 17 ms (98.17%) | Memory: 130.7 MB (59.14%) - LeetSync

--- 4213-merge-adjacent-equal-elements/merge-adjacent-equal-elements.java
+++ 4213-merge-adjacent-equal-elements/merge-adjacent-equal-elements.java
@@ -1,9 +1,8 @@
 class Solution {
     public List<Long> mergeAdjacent(int[] nums) {
        List<Long> stack = new ArrayList<>();
-
-        for (int num : nums) {
-            long cur = num;
+        for (int i=0;i<nums.length;i++) {
+            long cur = nums[i];
 
             while (!stack.isEmpty() && stack.get(stack.size() - 1) == cur) {
                 cur += stack.remove(stack.size() - 1);
