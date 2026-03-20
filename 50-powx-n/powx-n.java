c0a514c6cae579c61e50d87605e110dae685b21e
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-03-20 12:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (63.13%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,19 +1,24 @@
 class Solution {
+    public double power(double x, long binaryExpo){
+        if(binaryExpo==0){
+            return 1.0;
+        }
+        double half=power(x,binaryExpo/2);
+        //for even case
+        if(binaryExpo%2==0){
+            return half*half;
+        }
+        //for odd
+        else{
+            return half*half*x;
+        }
+    }
     public double myPow(double x, int n) {
-        long binaryForm=n;
+        long binaryExpo=n;
         if(n<0){
             x=1/x;
-            binaryForm=-binaryForm;
+            binaryExpo=-binaryExpo;
         }
-        double ans=1;
-        while(binaryForm>0){
-            if(binaryForm%2==1){
-                ans*=x;
-            }
-            x*=x;
-            binaryForm/=2;
-
-        }
-        return ans;
+        return power(x,binaryExpo);
     }
 }
