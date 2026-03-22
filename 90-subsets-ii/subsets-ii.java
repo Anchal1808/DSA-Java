c6e2af972649a81832fe1f09ef3dc352e3db8828
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-03-22 21:39:23 +0530

    Time: 2 ms (99.50%) | Memory: 44.9 MB (93.65%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,31 +1,26 @@
 class Solution {
+    public static void subset(int i,int[] arr,List<Integer> temp,List<List<Integer>> ans){
+        if(i==arr.length){
+            ans.add(new ArrayList(temp));
+            return;
+        }
+        //pick
+        temp.add(arr[i]);
+        subset(i+1,arr,temp,ans);
+        //backtrack
+        temp.remove(temp.size()-1);
+        // not pick
+        int j=i+1;
+        while(j<arr.length && arr[j]==arr[i]){
+            j++;
+        }
+        subset(j,arr,temp,ans);
+    }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-       List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> curr =new ArrayList<>();
-        helper(0,nums,curr,ans);
+         List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
+        subset(0,nums,temp,ans);
         return ans;
-    }
-    void helper(int i,int[] nums,List<Integer> curr, List<List<Integer>> ans){
-        //base case
-        if(i>=nums.length){
-            ans.add(new ArrayList<>(curr));
-            return ;
-        }
-        // pick
-        curr.add(nums[i]);
-         helper(i+1,nums,curr,ans);
-        //backtrack
-         curr.remove(curr.size()-1);
-           
-         //not pick skip all duplicates to check adjacent element
-         int j=i+1;
-         while(j<nums.length && nums[j]==nums[i]){
-            j++;
-         }
-         
-         helper(j,nums,curr,ans);
-
-
     }
 }
