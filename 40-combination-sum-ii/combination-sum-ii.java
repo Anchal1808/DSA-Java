aaeaea6ec926b903b08fa6f3ec02dd04744935b9
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-03-22 19:19:32 +0530

    Time: 6 ms (73.62%) | Memory: 45.3 MB (49.15%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,23 +1,22 @@
 class Solution {
-     public static void combination(int i,int[] arr,int target,List<Integer> temp,List<List<Integer>> ans){
-        
-            if(target==0){
-                ans.add(new ArrayList<>(temp));
+    public static void combination(int i,int[] arr,int sum,List<Integer> temp,List<List<Integer>> ans){
+        if(sum==0){
+            ans.add(new ArrayList(temp));
             return;
-            }
-        if(i==arr.length) return;
-        //pick
-        if(arr[i]<=target){ 
-        temp.add(arr[i]);
-         combination(i+1,arr,target-arr[i],temp,ans);
-         temp.remove(temp.size()-1);
         }
-        //not pick skip duplicate
+        if(i==arr.length){
+            return;
+        }
+        if(sum>=arr[i]){
+            temp.add(arr[i]);
+            combination(i+1,arr,sum-arr[i],temp,ans);
+            temp.remove(temp.size()-1);
+        }
         int j=i+1;
         while(j<arr.length && arr[j]==arr[i]){
             j++;
         }
-        combination(j,arr,target,temp,ans);
+        combination(j,arr,sum,temp,ans);
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
