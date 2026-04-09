c23fc17453087e69a61c41f70cfa7b32ac910437
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-04-09 23:15:49 +0530

    Time: 1 ms (95.20%) | Memory: 46.8 MB (48.58%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,25 +15,38 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans =new ArrayList<>();
-        if(root==null) return ans;
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
         Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
+        // loop till queue empty nh ho mtlb queue empty nh hai 
         while(!q.isEmpty()){
-            int size=q.size();
-            List<Integer> temp=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode curr=q.poll();
-               temp.add(curr.val);
-                if(curr.left!=null){
-                    q.add(curr.left);
-                }
-                if(curr.right!=null){
-                    q.add(curr.right);
-                }
-            }
-            ans.add(temp);
+            // it decide the loop till the elements in one level
+           int size=q.size();
+        //    becoz its only one time poll if size is 2 thats why need to change so it shoulb be inside loop
+        //    TreeNode curr=q.poll();
+           List<Integer> levelElement=new ArrayList<>();
+           for(int i=0;i<size;i++){
+            TreeNode curr=q.poll();
+            levelElement.add(curr.val);
+            if(curr.left!=null){
+            q.add(curr.left);
+           }
+           if(curr.right!=null){
+            q.add(curr.right);
+           }
+           }
+        //    kyuki curr change ho rha hai loop ke andr to value left right map nhnkar payega isliye ander hoga loop ke 
+        //    if(curr.left!=null){
+        //     q.add(curr.left);
+        //    }
+        //    if(curr.right!=null){
+        //     q.add(curr.right);
+        //    }
+           res.add(levelElement);
         }
-        return ans;
+        return res;
     }
 }
