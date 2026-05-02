f665cd1d055696bd0eca1a06ae037943425cdf34
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-05-02 20:19:11 +0530

    Time: 5 ms (82.96%) | Memory: 77.7 MB (12.51%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,25 +1,21 @@
 class Solution {
     public int maxArea(int[] height) {
-     int n=height.length;
-     int maxwater=0;
-     int i=0;
-     int j=n-1;
-    while(i<j){
-            int h=Math.min(height[i],height[j]);
-            int weidth=j-i;
-            int water=h*weidth;
-            maxwater=Math.max(maxwater,water);
-        
-        if(height[i]<height[j]){
-            i++;
+        int n=height.length;
+        int i=0;
+        int j=n-1;
+        int maxWater=Integer.MIN_VALUE;
+        while(i<j){
+            int ht=Math.min(height[i],height[j]);
+            int width=j-i;
+            int water=ht*width;
+            maxWater=Math.max(maxWater,water);
+            if(height[i]<height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
         }
-        else{
-            j--;
-        }
-    }
-     
-     return maxwater;
-
-
+        return maxWater;
     }
 }
