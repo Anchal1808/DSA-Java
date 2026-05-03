ef8da2145c773eb5d4850c83267c652bc8f02900
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-05-03 12:24:22 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (35.60%) - LeetSync

--- 4295-count-indices-with-opposite-parity/count-indices-with-opposite-parity.java
+++ 4295-count-indices-with-opposite-parity/count-indices-with-opposite-parity.java
@@ -2,37 +2,37 @@ class Solution {
     public int[] countOppositeParity(int[] nums) {
         int n=nums.length;
         int[] ans=new int[n];
-        for(int i=0;i<n;i++){
-            int cnt=0;
-            if(nums[i]%2!=0){
-                for(int j=i+1;j<n;j++){
-                    if(nums[j]%2==0){
-                        cnt++;
-                    }
-                }
-            }
-            else if(nums[i]%2==0){
-                for(int j=i+1;j<n;j++){
-                    if(nums[j]%2!=0){
-                        cnt++;
-                    }
-                }
-            }
-            ans[i]=cnt;
-        }
-        return ans;
-        // int evenCnt=0;
-        // int oddCnt=0;
-        // for(int i=n-1;i>=0;i--){
-        //     if(nums[i]%2==0){
-        //          ans[i]=evenCnt;
-        //         oddCnt++;
+        // for(int i=0;i<n;i++){
+        //     int cnt=0;
+        //     if(nums[i]%2!=0){
+        //         for(int j=i+1;j<n;j++){
+        //             if(nums[j]%2==0){
+        //                 cnt++;
+        //             }
+        //         }
         //     }
-        //     else if(nums[i]%2!=0){
-        //          ans[i]=oddCnt;
-        //         evenCnt++;
+        //     else if(nums[i]%2==0){
+        //         for(int j=i+1;j<n;j++){
+        //             if(nums[j]%2!=0){
+        //                 cnt++;
+        //             }
+        //         }
         //     }
+        //     ans[i]=cnt;
         // }
         // return ans;
+        int evenCnt=0;
+        int oddCnt=0;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]%2==0){
+                 ans[i]=evenCnt;
+                oddCnt++;
+            }
+            else if(nums[i]%2!=0){
+                 ans[i]=oddCnt;
+                evenCnt++;
+            }
+        }
+        return ans;
     }
 }
