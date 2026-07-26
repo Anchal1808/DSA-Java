5929e773585cd8095fe0c298669a76ca4745c2c3
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-07-26 21:04:25 +0530

    Time: 3 ms (86.33%) | Memory: 43.5 MB (15.63%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,29 +1,22 @@
 class Solution {
     boolean isOpen(char ch){
-        if(ch=='(' || ch=='{' || ch=='['){
-            return true;
-        }
-        return false;
+        return ch=='(' || ch=='[' || ch=='{';
     }
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<>();
-       for(int i=0;i<s.length();i++){
-        char ch=s.charAt(i);
-        if(isOpen(ch)){
-            st.push(ch);
-        }
-        else{
-            // closing ho par open nh hai
-            if(st.size()==0){
-                return false;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(isOpen(ch)){
+                st.push(ch);
             }
-             if((ch==')' && st.pop()!='(')||(ch==']' && st.pop()!='[')||(ch=='}' && st.pop()!='{')){
+            else{
+                if(st.size()==0) return false;
+                if((ch==')' && st.pop()!='(' )||(ch==']' && st.pop()!='[' )|| (ch=='}' && st.pop()!='{')){
                     return false;
                 }
-                
+            }
+
         }
-           
-       }
-       return st.isEmpty();
+        return st.isEmpty();
     }
 }
