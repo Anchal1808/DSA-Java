66c0d1d006d280152e69d9be6bb2737ffcef876d
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-07-27 10:15:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (99.55%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solve(int i,String s,int[] dp){
+        //string decoded
+        if(i==s.length()){
+            return 1;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        if(s.charAt(i)=='0'){
+            return 0;
+        }
+        int oneDigit=solve(i+1,s,dp);
+        int twoDigit=0;
+
+        if(i+1<s.length()){
+            int num=(s.charAt(i)-'0')*10+(s.charAt(i+1)-'0');
+        
+        if(num>=10&&num<=26){
+            twoDigit=solve(i+2,s,dp);
+        }
+        }
+        return dp[i]= oneDigit+twoDigit;
+
+    }
+    public int numDecodings(String s) {
+        int[] dp=new int[s.length()];
+        Arrays.fill(dp,-1);
+        return solve(0,s,dp);
+    }
+}
