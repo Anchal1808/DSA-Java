890814cb280ae2b5263f3d2945d580dc3295fefe
Anchal Dwivedi <anchaldwiedi015@gmail.com>
2026-07-28 09:56:54 +0530

    Time: 9 ms (43.48%) | Memory: 41.8 MB (93.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,10 +1,13 @@
 class Solution {
     int fibo(int n){
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
+        // if(n==0){
+        //     return 0;
+        // }
+        // if(n==1){
+        //     return 1;
+        // }
+        if(n<=1){
+            return n;
         }
         return fibo(n-1)+fibo(n-2);
     }
